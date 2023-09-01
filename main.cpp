dcfb9dca97ae2d6bd1d9f17dde722ccdec3cd39c
SaaFomenko <s.fomenko@list.ru>
2023-09-01 17:53:23 +0300

    Issue done.

--- main.cpp
+++ main.cpp
@@ -2,91 +2,117 @@
 #include <thread>
 #include <chrono>
 #include <vector>
+#include <atomic>
+//#include <mutex>
 
-const long sum_vec(const std::vector<int>& x, const std::vector<int>& y)
+
+//static std::mutex barrier;
+
+void sum_vec(
+    std::atomic<long>& result, 
+    const std::vector<int>& x, 
+    const std::vector<int>& y, 
+    const int l, 
+    const int r
+    )
 {
-    long result = 0;
+    long part_sum = 0;
 
-    for(int el : x)
-    {
-        result += el;
-    }
-    std::cout << "Summa x vector: " << result << '\n';
-    
-    for(int el : y)
-    {
-        result += el;
-    }
-    std::cout << "Summa x+y vectors: " << result << '\n';
+    for (int i = l; i < r; ++i)
+        part_sum += x.at(i) + y.at(i);
 
-    return result;
+   // std::lock_guard<std::mutex> block_threads_until_finish_this_job(barrier);
+
+    result += part_sum;
 }
 
-void vec_sum_v2(const std::vector<int>& x, long& result)
+std::vector<int> bounds(const int thr, const int num_elem)
 {
-    for (int el : x)
+    std::vector<int> bnd;
+    int delta = num_elem / thr;
+    int reminder = num_elem % thr;
+    int n1 = 0;
+    int n2 = 0;
+
+    bnd.push_back(n1);
+
+    for (int i = 0; i < thr; ++i)
     {
-        result += el;
+        n2 = n1 + delta;
+
+        if (i == thr - 1)
+            n2 += reminder;
+
+        bnd.push_back(n2);
+        n1 = n2;
     }
+
+    return bnd;
+}
+
+void print_thr_test(const int num_elem, const int thr)
+{
+    std::vector<int> x;
+    std::vector<int> y;
+    std::vector<std::thread> V;
+    std::atomic<long> result(0);
+    std::vector<int> bnd(bounds(thr, num_elem));
+    
+    for (int k = 0; k < num_elem; ++k)
+    {
+        x.push_back(k);
+        y.push_back(k);
+    }
+    
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < thr; ++i)
+    {
+        V.push_back(std::thread(
+            sum_vec, 
+            std::ref(result), 
+            std::ref(x), 
+            std::ref(y), 
+            bnd.at(i), 
+            bnd.at(i + 1)
+        ));
+    }
+    
+    for(auto &el : V)
+    {
+        if (el.joinable())
+            el.join();
+    }
+    
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    
+    std::cout << '\t' << elapsed_seconds.count() << 's';
 }
 
 int main()
 {
-    //const int thr_1[]{1, 2, 4, 8, 16};
+    const int num_elem[] = {1000, 10000, 100000, 1000000};
+    const int thr[] = {1, 2, 4, 8, 16};
+    const int thr_size = sizeof(thr) / sizeof(thr[0]);
+    const int num_size = sizeof(num_elem) / sizeof(num_elem[0]);
 
-    //const int mill = 1000000;
-    const int mill = 1000000;
-    const int thr = 2;
+    std::cout << "CPU logical cores: " << std::thread::hardware_concurrency() << "\n\n\t";
 
-    std::cout << "mill = " << mill << '\n';
-
-    std::vector<int> x;
-    std::vector<int> y;
-    std::vector<std::thread> V;
-
-    for (int i = 0; i < mill; ++i)
+    for (int i = 0; i < num_size; ++i)
     {
-        x.push_back(i);
-        y.push_back(i);
+        std::cout << '\t' << num_elem[i] << '\t';
     }
-
-    auto start = std::chrono::steady_clock::now();
-
-    long result = sum_vec(x, y);
-
-    auto end = std::chrono::steady_clock::now();
-
-    auto delta = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-    //double exec_time = static_cast<double>(delta.count() / 1000000.0);
-    double exec_time = delta.count() / 1000000.0;
-
-    std::cout.precision(12);
-    std::cout << "Time sum 2 vectors[" << mill - 1 << "] = " << exec_time << " sec. \n";
-    
-    std::cout << "Last element vector[" << mill - 1 << "] = " << *--x.end() << '\n';
-
-    std::cout << "Summa vectors x and y = " << result << '\n';
-    std::cout << "Size result in byte: " << sizeof(result) << "\n\n\n";
-
-    result = 0;
-
-    start = std::chrono::steady_clock::now();
-    
-    for (int i = 0; i < thr; ++i)
+    std::cout << '\n';
+   
+    for (int i = 0; i < thr_size; ++i)
     {
-        bool isNull = i % 2 == 0;
-        if (isNull)
-            V.push_back(std::thread(vec_sum_v2, ref(x), std::ref(result)));
-        else
-            V.push_back(std::thread(vec_sum_v2, ref(y), std::ref(result)));
+        std::cout << thr[i] << " threads";
+        for (int j = 0; j < num_size; ++j)
+        {
+            print_thr_test(num_elem[j], thr[i]);
+        }
+        std::cout << '\n';
     }
-
-    end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-
-    std::cout << "Result thr_sum_vec with 2 threads: " << result << '\n';
-    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
     return 0;
 }
